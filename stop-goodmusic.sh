85c9e756c85305da394213afb1cf3d7c929854a3
riccardo dominici <riky.jak7@gmail.com>
2025-02-09 16:07:40 +0100

    Rilascio: ogni servizio è ora eseguito in un container docker

--- stop-goodmusic.sh
+++ stop-goodmusic.sh
@@ -17,3 +17,16 @@ docker stop db-recensioni
 docker rm db-recensioni
 docker stop db-connessioni
 docker rm db-connessioni
+
+#termino i container dei microservizi
+docker stop recensioni
+docker rm recensioni
+docker stop connessioni
+docker rm connessioni
+docker stop recensioni-seguite
+docker rm recensioni-seguite
+docker stop api-gateway
+docker rm api-gateway
+
+#elimina la rete
+docker network rm goodmusic-network
